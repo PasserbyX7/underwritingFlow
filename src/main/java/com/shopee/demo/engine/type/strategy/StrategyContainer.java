ab929e00fe85713885ca1a131e430f83d3ca8798
Li Cao <li.cao@shopee.com>
2022-05-08 21:14:03 +0800

    fix bug

--- src/main/java/com/shopee/demo/engine/type/strategy/StrategyContainer.java
+++ src/main/java/com/shopee/demo/engine/type/strategy/StrategyContainer.java
@@ -22,6 +22,6 @@ public class StrategyContainer<T> {
 
     public static <T> StrategyContainer<T> fromJson(String str) {
         // TODO
-        return null;
+        return new StrategyContainer<>();
     }
 }
