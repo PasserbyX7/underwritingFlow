e6321573949559439d3c122f600c404f42f25898
Passerby <756635176@qq.com>
2022-05-15 01:12:18 +0800

    代码修改

--- src/main/java/com/shopee/demo/engine/type/strategy/sme/input/strategy1/SmeStrategy1InitiationInput.java
+++ src/main/java/com/shopee/demo/engine/type/strategy/sme/input/strategy1/SmeStrategy1InitiationInput.java
@@ -1,8 +1,10 @@
 package com.shopee.demo.engine.type.strategy.sme.input.strategy1;
 
+import com.shopee.demo.engine.type.strategy.StrategyInput;
+
 import lombok.Data;
 
 @Data
-public class SmeStrategy1InitiationInput {
+public class SmeStrategy1InitiationInput implements StrategyInput {
 
 }
